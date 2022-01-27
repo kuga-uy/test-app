e820b62fcd6719d1d3aaf2e6ebe3978fd311f2c1
Eduardo Diaz <eduardo.diaz@endava.com>
2022-01-27 19:55:35 -0300

    config example with react and ts

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,32 @@ import { useState } from "react";
 function App() {
   const [task, setTask] = useState<Array<string>>([""]);
   const [value, setValue] = useState<string>('');
+  const [disabled, setDisabled] = useState<boolean>(true)
 
   const addTask = () => {
-    setTask([...task, value]);
-    setValue("");
+   
+      setDisabled(false)
+      setTask([...task, value]);
+      setValue("");
+
+    
   };
 
   const valueHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value);
+    setValue(e.target.value)
+    e.target.value.length > 2
+    ? setDisabled(false) 
+    : setDisabled(true)
   };
 
   console.log(task);
   return (
     <div className="App">
       <input onChange={valueHandler} type="text" />
-      <button onClick={addTask}>add task</button>
+      <button onClick={addTask}disabled={disabled}>add task</button>
       <ul>
         {task.map((taskItem, i) => (
-          <li style={ { listStyle: "none" } }key={i}>{taskItem.length > 0 ? `${i} - ${taskItem}` : ""}</li>
+          <li style={ { listStyle: "none" } }key={i} value={value}>{`${i} - ${taskItem}`}</li>
         ))}
       </ul>
     </div>
